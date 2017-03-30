c78355b92c229053d5a4c6bf5cd55090681492dc
Nick Lee <nicklee808@gmail.com>
2017-03-29 20:36:42 -1000

    fixing

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,3 +1,27 @@
 const BookListArray = [
-
+  {
+    _id: 0,
+    title: 'The Book',
+    author: 'Alan Watts'
+  },
+  {
+    _id: 1,
+    title: 'Be Here Now',
+    author: 'Ram Dass'
+  },
+  {
+    _id: 2,
+    title: 'Do Androids Dream of Electric Sheep?',
+    author: 'Philip K. Dick'
+  },
+  {
+    _id: 3,
+    title: 'Haunted',
+    author: 'Chuck Palahniuk'
+  },
+  {
+    _id: 4,
+    title: 'The Stinky Cheese Man and Other Fairly Stupid Tales',
+    author: 'Jon Scieszka'
+  }
 ];
